a95143fc0e4b597ef26c370921cec690eebb99be
Your Name <you@example.com>
2019-04-04 09:19:57 +0100

    The Completed alternatingCharacters function

--- test.js
+++ test.js
@@ -1,19 +1,25 @@
-const item = 'AAABBBABBABABABABABBABAB';
-let newItem = item.split('');
-let string = '';
-let numberOfDeletions = 0;
-if (newItem.length > 0) {
-  let prev = newItem[0];
-  string += prev
-  for (let i = 1; i < newItem.length; i++) {
-      let cur = newItem[i];
-      if (cur != prev) {
-        string += cur;
-        prev = cur;
-      } else {
-        numberOfDeletions+=1;
-      }
-  };
-  console.log(`New Item ===> ${string}`);
-  console.log(`${numberOfDeletions} deletions`);
-};
+// Complete the alternatingCharacters function below.
+function alternatingCharacters(s) {
+    const item = s;
+    let newItem = item.split('');
+    let string = '';
+    let numberOfDeletions = 0;
+    if (newItem.length > 0) {
+        let prev = newItem[0];
+        string += prev
+        for (let i = 1; i < newItem.length; i++) {
+            let cur = newItem[i];
+            if (cur != prev) {
+                string += cur;
+                prev = cur;
+            } else {
+                numberOfDeletions += 1;
+            }
+        };
+
+        console.log(`New Item ===> ${string}`);
+        console.log(`${numberOfDeletions} deletions`);
+    };
+
+    return numberOfDeletions;
+}
